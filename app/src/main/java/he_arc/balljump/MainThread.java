b57f56811d968f4f1cb8077f43f695cb4d73721c
Mael Pedretti <mael.pedretti@hotmail.fr>
2017-11-17 09:45:39 +0100

    Blocked fps. Blocked screen refresh frequency using delays. Here 30 fps.

--- app/src/main/java/he_arc/balljump/MainThread.java
+++ app/src/main/java/he_arc/balljump/MainThread.java
@@ -4,8 +4,11 @@ import android.graphics.Canvas;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.SurfaceHolder;
+import 	android.os.SystemClock;
 
 import java.io.Console;
+
+import static android.os.SystemClock.uptimeMillis;
 
 /**
  * Created by pedrocosta on 28.10.17.
@@ -25,6 +28,13 @@ public class MainThread extends Thread {
 
     @Override
     public void run() {
+        final int FRAMES_PER_SECOND = 30;
+        final int SKIP_TICKS = 1000 / FRAMES_PER_SECOND;
+        long next_game_tick = uptimeMillis();
+        // uptimeMillis() returns the current number of milliseconds
+        // that have elapsed since the system was started
+
+        long sleep_time = 0;
         while (running){
             canvas = null;
             try {
@@ -33,6 +43,14 @@ public class MainThread extends Thread {
                 synchronized (this.surfaceHolder){
                     this.gamePanel.update();
                     this.gamePanel.draw(canvas);
+                }
+                next_game_tick += SKIP_TICKS;
+                sleep_time = next_game_tick - uptimeMillis();
+                if( sleep_time >= 0 ) {
+                    Thread.sleep( sleep_time );
+                }
+                else {
+                    System.out.println("WE ARE LATE");
                 }
             }catch (Exception e){
                 e.printStackTrace();
