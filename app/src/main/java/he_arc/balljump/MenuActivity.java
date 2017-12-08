0c8e5ab0409719ee49211a18f11a0421ef5c913d
Thibaut <thibaut.piquerez@he-arc.ch>
2017-12-08 11:07:17 +0100

    ajout du menu

---
+++ app/src/main/java/he_arc/balljump/MenuActivity.java
@@ -0,0 +1,27 @@
+package he_arc.balljump;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        Button button = findViewById(R.id.buttonPlay);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent i = new Intent(MenuActivity.this, Game.class);
+                startActivity(i);
+            }
+        });
+    }
+}
